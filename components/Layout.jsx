f9d0f84c6737b87fbe36d19034f70bff29f4e7a5
HabacucMX <habacuc.vera@gmail.com>
2022-03-19 05:45:47 -0600

    Hud elements, no Header needed

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,18 +3,51 @@ import Account from "./Account/Account";
 
 export default function Layout({ children, home }) {
   return (
+
     <div>
-      <nav className="container bg-gray-300 py-5">
-        <Account />
-      </nav>
-      <main>{children}</main>
-      {!home && (
-        <div>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
-    </div>
+    <nav >
+      <div><a>Twitter</a></div>
+      <div>Location</div>
+      <div> ENS / <Account /></div>
+      <div>Balance</div>
+      <div> ⚡ Emergy Units</div>
+      <div> 💽 Chips Units</div>
+      <div> 🪨 Carbon</div>      
+    </nav>
+    <main>{children}</main>
+    {!home && (
+      <div>
+        <Link href="/">
+          <a>← Back to home</a>
+        </Link>
+      </div>
+    )}
+    <nav>
+      <div ><a>Docs</a></div>
+      <div></div>
+      <div ><a>Discord</a></div>
+    </nav>
+  </div>
+ 
   );
+    
+  
+  //return (
+  //  <div>
+
+  //    <nav className="container bg-gray-300 py-5">
+  //      <Account />
+  //    </nav>
+
+
+  //    <main>{children}</main>
+  //    {!home && (
+  //      <div>
+  //        <Link href="/">
+  //          <a>← Back to home</a>
+  //        </Link>
+  //      </div>
+  //    )}
+  //  </div>
+  //);
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import "../styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apolloClient";
-import Head from "next/head";
 import dynamic from "next/dynamic";
+
+
 
 const MoralisContextProvider = dynamic(() => import("../components/MoralisContext"), {
   ssr: false,
@@ -14,10 +15,10 @@ export default function App({ Component, pageProps }) {
   return (
     <MoralisContextProvider>
       <ApolloProvider client={apolloClient}>
-        <Head>
+        
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Solarpucks</title>
-        </Head>
+          <title>Solarpunks</title>
+        
         <Component {...pageProps} />
       </ApolloProvider>
     </MoralisContextProvider>
