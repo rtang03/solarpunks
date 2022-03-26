ee22b7fa4cb6681762c6da9e6f616a334435bee6
rtang03 <7686266+rtang03@users.noreply.github.com>
2022-03-26 13:32:46 +0800

    Merge branch 'master' of https://github.com/rtang03/solarpunks

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -7,6 +7,7 @@ import { useMoralis } from "react-moralis";
 import { useQueryPuckCitiesMetadata } from "../hooks/useQueryPuckCitiesMetadata";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import Image from "next/image";
+import Pagination from "../components/Pagination";
 
 const PUNKCITIES_ADDRESS = "0x092BBe9022D421940B6D74799179267e5c822895";
 
@@ -19,20 +20,35 @@ const Dashboard = () => {
   const [city3, setCity3] = useState();
   const [city4, setCity4] = useState();
   const [city5, setCity5] = useState();
+  const [cities, setCities] = useState({});
+  const [citiesArray, setCitiesArray] = useState();
+  const [currentCity, setCurrentCity] = useState(0);
+
+  const numberOfCities = citiesArray?.length;
+
+  useEffect(() => {
+    city0 && setCities({ ...cities, [city0.tokenID]: city0 });
+    city1 && setCities({ ...cities, [city1.tokenID]: city1 });
+    city2 && setCities({ ...cities, [city2.tokenID]: city2 });
+    city3 && setCities({ ...cities, [city3.tokenID]: city3 });
+    city4 && setCities({ ...cities, [city4.tokenID]: city4 });
+    city5 && setCities({ ...cities, [city5.tokenID]: city5 });
+    cities && setCitiesArray(Object.values(cities));
+  }, [city0, city1, city2, city3, city4, city5]);
 
   // query cities
   const { citiesMetadataloading: loading0, citiesMetadataError: error0 } =
     useQueryPuckCitiesMetadata(setCity0, PUNKCITIES_ADDRESS, 0);
   const { citiesMetadataloading: loading1, citiesMetadataError: error1 } =
-    useQueryPuckCitiesMetadata(setCity0, PUNKCITIES_ADDRESS, 1);
+    useQueryPuckCitiesMetadata(setCity1, PUNKCITIES_ADDRESS, 1);
   const { citiesMetadataloading: loading2, citiesMetadataError: error2 } =
-    useQueryPuckCitiesMetadata(setCity0, PUNKCITIES_ADDRESS, 2);
+    useQueryPuckCitiesMetadata(setCity2, PUNKCITIES_ADDRESS, 2);
   const { citiesMetadataloading: loading3, citiesMetadataError: error3 } =
-    useQueryPuckCitiesMetadata(setCity0, PUNKCITIES_ADDRESS, 3);
+    useQueryPuckCitiesMetadata(setCity3, PUNKCITIES_ADDRESS, 3);
   const { citiesMetadataloading: loading4, citiesMetadataError: error4 } =
-    useQueryPuckCitiesMetadata(setCity0, PUNKCITIES_ADDRESS, 4);
+    useQueryPuckCitiesMetadata(setCity4, PUNKCITIES_ADDRESS, 4);
   const { citiesMetadataloading: loading5, citiesMetadataError: error5 } =
-    useQueryPuckCitiesMetadata(setCity0, PUNKCITIES_ADDRESS, 5);
+    useQueryPuckCitiesMetadata(setCity5, PUNKCITIES_ADDRESS, 5);
 
   error0 && console.error("fail to fetch city0", error0);
   error1 && console.error("fail to fetch city1", error1);
@@ -41,7 +57,9 @@ const Dashboard = () => {
   error4 && console.error("fail to fetch city4", error4);
   error5 && console.error("fail to fetch city5", error5);
 
-  console.log(city0);
+  const next = () => setCurrentCity((currentCity + 1) % numberOfCities);
+  const prev = () => setCurrentCity((currentCity + numberOfCities - 1) % numberOfCities);
+
   return (
     <Layout>
       <div className="MainCon">
@@ -75,40 +93,46 @@ const Dashboard = () => {
               {" "}
               <div className="my-5">Punk Cities Places</div>
               <div className="text-md py-2">
-                <div className="border-2 text-left m-10">
-                  <div>Name: {city0.name}</div>
-                  <div>Desc: {city0.description}</div>
-                  {city0.content && <div>Content: {city0.content}</div>}
-                  <div>Tag: {city0.tag}</div>
-                  <div>TokenID: {city0.tokenID}</div>
-                  <div className="flex flex-row">
-                    <span>
-                      <FaMapMarkerAlt />
-                    </span>
-                    <span>
-                      <a
-                        className="m-2 p-2 underline text-night-100 hover:text-solar-100"
-                        target="_blank"
-                        rel="noreferrer"
-                        href={city0.address}
-                      >
-                        Map
-                      </a>
-                    </span>
-                  </div>
-                  <div className="font-bold my-2">Attributes</div>
-                  {city0.attributes?.map((attribute, index) => (
-                    <div key={index}>
-                      <div className="ml-5">trait_type: {attribute.trait_type}</div>
-                      <div className="ml-5 mb-2">value: {attribute.value}</div>
+                {numberOfCities >= 0 && (
+                  <div className=" text-left m-10">
+                    <div>Name: {citiesArray?.[currentCity]?.name}</div>
+                    <div>Desc: {citiesArray?.[currentCity]?.description}</div>
+                    <div>Content: {citiesArray?.[currentCity]?.content}</div>
+                    <div>Tag: {citiesArray?.[currentCity]?.tag}</div>
+                    <div>TokenID: {citiesArray?.[currentCity]?.tokenID}</div>
+                    <div className="flex flex-row">
+                      <span>
+                        <FaMapMarkerAlt />
+                      </span>
+                      <span>
+                        <a
+                          className="m-2 p-2 underline text-night-100 hover:text-solar-100"
+                          target="_blank"
+                          rel="noreferrer"
+                          href={citiesArray?.[currentCity]?.address}
+                        >
+                          Map
+                        </a>
+                      </span>
                     </div>
-                  ))}
-                  <Image
-                    width={256}
-                    height={256}
-                    src={city0.image.replace("ipfs://", "https://ipfs.io/ipfs/")}
-                  />
-                </div>
+                    <div className="font-bold my-2">Attributes</div>
+                    {citiesArray?.[currentCity]?.attributes?.map((attribute, index) => (
+                      <div key={index}>
+                        <div className="ml-5">trait_type: {attribute.trait_type}</div>
+                        <div className="ml-5 mb-2">value: {attribute.value}</div>
+                      </div>
+                    ))}
+                    <Image
+                      width={256}
+                      height={256}
+                      src={citiesArray?.[currentCity]?.image.replace(
+                        "ipfs://",
+                        "https://ipfs.io/ipfs/",
+                      )}
+                    />
+                    <Pagination next={next} prev={prev} totalCount={numberOfCities || 0} />
+                  </div>
+                )}
               </div>
             </div>
             <div className="Board2">My Followers</div>
