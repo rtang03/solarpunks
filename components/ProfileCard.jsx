12d22554d29fdbddd6685b9719bfed27b94ef149
HabacucMX <habacuc.vera@gmail.com>
2022-03-25 19:08:40 -0600

    Profile detail styles as classes and breakpoints

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -14,43 +14,49 @@ const ProfileCard = ({
 
   return (
     <div className="font-exo">
-      <div className="grid grid-cols-2">
+      <div className="ProfileGrid">
+
+        
         <div className="justify-self-center">
-          <img className="h-96 w-96 rounded-lg bg-glass-100 mb-20 " src={coverPicUrl} />
+          <img className="ProfilePic" src={coverPicUrl} />
         </div>
-        <div className="text-left text-lg mt-3 -m-16">
-          <div className="text-3xl text-night-100 mb-3 pr-16 text-left">
+
+
+        <div className="">
+          <div className="ProfileData">
             <div className="my-3">☀️ {profile.name}</div>
             <div className="my-3">
               🌿{profile.handle}#{profile.profileId}
             </div>
-            <div className="my-3">🐋 @{profile.twitterUrl}</div>
-            <div className="my-3">🌐 {profile.website}</div>
-            <span>🗺️ {profile.location}</span>
+            <div className="my-3 text-lg">🐋 @{profile.twitterUrl}</div>
+            <div className="my-3 text-lg">🌐 {profile.website}</div>
+            <div className="text-lg">🗺️ {profile.location}</div>
             <div className="text-base my-3">{profile.ownedBy}</div>
           </div>
           <div className="text-lg mt-3 mb-10">{profile.bio}</div>
         </div>
-        <div className="col-span-2 grid grid-cols-4 text-xl gap-3 text-white">
+
+
+        <div className="StatsGrid">
           <div
-            className="bg-glass-100 rounded-lg hover:cursor-pointer"
+            className="StatsBg-Pointer"
             onClick={openFollowersDialogModal}
           >
             <div className="text-5xl m-3">🪴</div>Followers: {stats?.totalFollowers}
           </div>
           <div
-            className="bg-glass-100 rounded-lg hover:cursor-pointer"
+            className="StatsBg-Pointer"
             onClick={openFollowingsDialogModal}
           >
             <div className="text-5xl m-3">🍃</div> Following: {stats?.totalFollowing}
           </div>
-          <div className="bg-glass-100 rounded-lg">
+          <div className="StatsBg">
             <div className="text-5xl m-3">🌱</div> Posts: {stats?.totalPosts}
           </div>
-          <div className="bg-glass-100 rounded-lg">
+          <div className="StatsBg">
             <div className="text-5xl m-3">🪞</div> Mirrors: {stats?.totalMirrors}
           </div>
-          <div className="bg-glass-100 rounded-lg">
+          <div className="StatsBg">
             <div className="text-5xl m-3">🎁</div> Collects: {stats?.totalCollects}
           </div>
           <Link
@@ -60,13 +66,11 @@ const ProfileCard = ({
                 : `/profiles/${handle}/publications`
             }
           >
-            <a>
-              <div className="bg-glass-100 rounded-lg hover:cursor-pointer">
+              <div className="StatsBg-Pointer">
                 <div className="text-5xl m-3">📜</div> Publications: {stats?.totalPublications}
               </div>
-            </a>
           </Link>
-          <div className="bg-glass-100 rounded-lg">
+          <div className="StatsBg">
             <div className="text-5xl m-3">💬</div> Comments: {stats?.totalComments}
           </div>
         </div>
